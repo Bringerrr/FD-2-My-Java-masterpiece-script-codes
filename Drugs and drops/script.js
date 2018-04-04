4a9144caccdee5a42a4fb500bed3d460312af208
Bringerrr <31519731+Bringerrr@users.noreply.github.com>
2018-04-04 22:08:46 +0200

    Add files via upload

--- Drugs and drops/script.js
+++ Drugs and drops/script.js
@@ -1,45 +1,34 @@
-var mainBoard = document.getElementById("main-board");
-var boardForPieces = document.getElementById("board-for-pieces");
-var boardClear = document.getElementById("board-clear");
-var picture = document.getElementsByTagName("img");
-var imgSrc = picture[0].src;
+var mainBoard = document.getElementById("main-board"),
+    boardForPieces = document.getElementById("board-for-pieces"),
+    boardClear = document.getElementById("board-clear"),
+    picture = document.getElementsByTagName("img"),
+    imgSrc = picture[0].src, // адрес первой картинки
+    pieces = boardForPieces.getElementsByTagName("div"),
+    piecesInPurple = boardClear.getElementsByTagName("div"),
+    piecesCountX = 4, piecesCountY = 4 , // Количество кусочков -------------
+    posSpriteX = 0, posSpriteY = 0, // Координа для спратов
 
-var pieces = boardForPieces.getElementsByTagName("div");
+    str = "", // Переменная для innerHTML
 
-var piecesInPurple = boardClear.getElementsByTagName("div");
+    boardForPiecesPos=getElementPos(boardForPieces), // Получили координаты фиолетового блока
 
+    draggedPiece=null, //Событийная переменная
+    index = 1; //Переменная для изменения z-index
 
-
-var piecesCountX = 4, // Количество кусочков -------------
-    piecesCountY = 4
-
-
-var posSpriteX = 0;
-var posSpriteY = 0;
-
-
-// Установка параметров для куска картинки -------------
+// Установка размеров куска картинки -------------
 var Image = {
     width : picture[0].width / piecesCountX ,
     height : picture[0].height / piecesCountY 
 }
 
-console.log(Image);
-
-
 // Создание досок для кусочков и пустой доски -------------
 boardForPieces.style.width= Image.width*piecesCountX + "px"
 boardForPieces.style.height= Image.height*piecesCountY + "px"
 
-boardClear.style.width= Image.width*piecesCountX + "px"
+boardClear.style.width= Image.width*piecesCountX+ "px"
 boardClear.style.height= Image.height*piecesCountY + "px"
 
-
-
-
 // Создание блоков (кусков мозайки) -------------
-
-var str = "";
 
 for(i=0; i<piecesCountY*piecesCountX;i++){
     str+="<div></div>"
@@ -47,10 +36,7 @@ for(i=0; i<piecesCountY*piecesCountX;i++){
 
 boardForPieces.innerHTML = str;
 
-// Присваивание стилей блокам -------------
-
-
-
+// Назначение стилей блокам -------------
 
 for(i=0; i<(piecesCountY*piecesCountX);i++){
     pieces[i].style.float="left";
@@ -62,26 +48,18 @@ for(i=0; i<(piecesCountY*piecesCountX);i++){
 
     pieces[i].style.outline= "5px solid black";
 
-    pieces[i].id=i; // присваиваие айдишника
-
+    pieces[i].id=i; // присваивание айдишника (на всякий случай)
 }
 
 // Расстановка координат для спрайтов и самих блоков -------------
 
-var boardForPiecesPos=getElementPos(boardForPieces);
-// Получили координаты блока для кусочков с помощью функции getElementPos()
-
-
 for(i=0; i<(piecesCountY*piecesCountX);i++){ 
     pieces[i].style.backgroundPositionX = -posSpriteX + "px";
-    pieces[i].style.backgroundPositionY = -posSpriteY + "px";
-    // Выставили позицию для спрайтов
+    pieces[i].style.backgroundPositionY = -posSpriteY + "px";// Выставили позицию для спрайтов
 
-    pieces[i].style.position="absolute"
-    pieces[i].style.left = boardForPiecesPos.left + posSpriteX + "px";
-    pieces[i].style.top = boardForPiecesPos.top + posSpriteY + "px";
-    // задали абсолютное позиционирование 
-    // задали абсолютное позиционирование 
+    pieces[i].style.position="absolute" // задали абсолютное позиционирование 
+    pieces[i].style.left = boardForPiecesPos.left + posSpriteX + "px";    
+    pieces[i].style.top = boardForPiecesPos.top + posSpriteY + "px";  // задали координаты
 
     if (posSpriteX<Image.width*(piecesCountX-1)) {
         posSpriteX += Image.width;
@@ -92,96 +70,77 @@ for(i=0; i<(piecesCountY*piecesCountX);i++){
     }
 }
 
-// События ----------------
+// События ------------------------------
 
 for(i=0; i<(piecesCountY*piecesCountX);i++){ 
     pieces[i].addEventListener('dragstart',pieceDragStart,false);
     pieces[i].addEventListener('drop',pieceDragEnd,false);
-    pieces[i].addEventListener('click',boxClicked,false); // Test
     // pieces[i].addEventListener('dragover',divDragOver,false);
-
+    pieces[i].addEventListener('click',clickIndex,false);
 }
 
 boardForPieces.addEventListener('dragover',divDragOver,false);
 boardForPieces.addEventListener('drop',divDrop,false);
+
 boardClear.addEventListener('dragover',divDragOver,false);
 boardClear.addEventListener('drop',divDrop,false);
 
-var draggedPiece=null;
+function clickIndex(EO){
+    EO=EO||window.event;
+    index +=1;
+    EO.currentTarget.style.zIndex=index;
+}
 
-function pieceDragStart(EO) {
+function pieceDragStart(EO) { // Хватаем кусочек
     EO=EO||window.event;
     console.log('starting drag id='+EO.currentTarget.id);
     draggedPiece=EO.currentTarget;
-    draggedPiece.style.zIndex="39";
-
-            //Test
-
     // найдём положение самого жёлтого квадрата относительно страницы
-    var boxPos=getElementPos(draggedPiece);
-    // console.log(EO.currentTarget)
+    var boxPosX=getElementPos(draggedPiece);
     var infoObj=document.getElementById('IInfo');
-    // найдём координаты клика относительно жёлтого квадрата
-    var clickX=Math.round(EO.pageX-boxPos.left);
-    var clickY=Math.round(EO.pageY-boxPos.top);
-    infoObj.innerHTML="x="+clickX+" y="+clickY;
-    return clickX
+    // находим координаты клика относительно жёлтого квадрата
+    var clickXC=Math.round(EO.pageX-boxPosX.left);
+    var clickYC=Math.round(EO.pageY-boxPosX.top);
+    console.log({clickXC,clickYC});
 }
 
-function pieceDragEnd(EO) {
+function pieceDragEnd(EO) { // Сбрасываем кусочек
     // закончилось перетаскивание мячика (неважно куда он уронен)
     EO=EO||window.event;
     console.log('drag finished');
-    console.log(piecesInPurple)
-    
     draggedPiece=null;
 }
 
-function divDragOver(EO) {
+function divDragOver(EO) { // Позволяем сбросить кусочек в блок
     EO=EO||window.event;
     // по-умолчанию ронять элементы в div запрещено, отменяем:
     EO.preventDefault();
 }
 
-function divDrop(EO) {
+function divDrop(EO) {  // Сбрасываем кусочек в блок
     // мячик уронен
     EO=EO||window.event;
     EO.preventDefault();
     if ( draggedPiece ){
-        // if (EO.currentTarget.id==15){
-        //     console.log("u got it!")
-        // }
-        console.log(EO.currentTarget.id)
+        console.log(EO.currentTarget.id);
         EO.currentTarget.appendChild(draggedPiece);
-
-        console.log(EO.currentTarget);
-        var boxPos=getElementPos(boardClear);
-
-        
+        // var boxPos=getElementPos(draggedPiece);
         var infoObj=document.getElementById('IInfo');
         // найдём координаты клика относительно жёлтого квадрата
         var clickX=Math.round(EO.pageX);
         var clickY=Math.round(EO.pageY);
         infoObj.innerHTML="x="+clickX+" y="+clickY;
-        // console.log(getElementPos)
+        
         draggedPiece.style.position="absolute";
         draggedPiece.style.top=clickY+"px";
         draggedPiece.style.left=clickX+"px";
+
+        index +=1;
+        EO.currentTarget.style.zIndex=index;
     }
 }
 
-function boxClicked(EO) {
-    EO=EO||window.event;
-    // найдём положение самого жёлтого квадрата относительно страницы
-    var boxPos=getElementPos(EO.currentTarget);
-    // console.log(EO.currentTarget)
-    var infoObj=document.getElementById('IInfo');
-    // найдём координаты клика относительно жёлтого квадрата
-    var clickX=Math.round(EO.pageX-boxPos.left);
-    var clickY=Math.round(EO.pageY-boxPos.top);
-    infoObj.innerHTML="x="+clickX+" y="+clickY;
-}
-
+// Функция дял установки координат ------------
 function getElementPos(elem) {
     var bbox=elem.getBoundingClientRect();
     return {
