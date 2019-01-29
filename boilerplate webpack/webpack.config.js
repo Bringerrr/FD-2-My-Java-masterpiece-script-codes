6eb004625d1e7aae024082156fbc987ee7b5e7c2
Bringerrr <31519731+Bringerrr@users.noreply.github.com>
2019-01-29 17:42:57 +0300

    Add files via upload

---
+++ boilerplate webpack/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+  devServer: {
+    port: 9000
+  },
+  entry: ['@babel/polyfill', './src/index.js'],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.js$/,
+        use: ['babel-loader'],
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loader: 'file-loader',
+        options: { name: 'img/[name].[ext]' }
+      },
+      { test: /\.handlebars$/, loader: 'handlebars-loader' }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    inject: false,
+    hash: true,
+    template: "./src/index.html",
+    filename: "index.html"
+  })]
+}
